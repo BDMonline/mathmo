84259e7ed035ff151443293a2943795d29f0058e
Brian Medley <brian.spam@ntlworld.com>
2016-02-15 13:09:26 +0000

    initialise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mathmo::Application.config.secret_token = '7399fecddc0df291321bfed47fd19cd63f2163598eb6c9acd2a0c02d810956936ae050fcad16ffab718835b082887d582922a5565fed2bac87311bfac91a9c21'
